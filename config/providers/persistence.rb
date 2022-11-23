9601fed275807713a66185f1bc8afc689284ba4b
Tim Riley <tim@riley.id.au>
2022-11-23 15:33:24 +1100

    Use Hanami 2.0.0 (#8)

--- config/providers/persistence.rb
+++ config/providers/persistence.rb
@@ -6,21 +6,22 @@ Hanami.app.register_provider :persistence, namespace: true do
     require "rom/core"
     require "rom/sql"
 
-    @config = ROM::Configuration.new(:sql, target["settings"].database_url)
+    rom_config = ROM::Configuration.new(:sql, target["settings"].database_url)
 
-    config.plugin(:sql, relations: :instrumentation) do |plugin_config|
+    rom_config.plugin(:sql, relations: :instrumentation) do |plugin_config|
       plugin_config.notifications = target["notifications"]
     end
 
-    config.plugin(:sql, relations: :auto_restrictions)
+    rom_config.plugin(:sql, relations: :auto_restrictions)
 
-    register "config", config
-    register "db", config.gateways[:default].connection
+    register "config", rom_config
+    register "db", rom_config.gateways[:default].connection
   end
 
   start do
-    @config.auto_registration target.root.join("lib/decafsucks/persistence")
+    rom_config = target["persistence.config"]
+    rom_config.auto_registration target.root.join("lib/decafsucks/persistence")
 
-    register "rom", ROM.container(@config)
+    register "rom", ROM.container(rom_config)
   end
 end
