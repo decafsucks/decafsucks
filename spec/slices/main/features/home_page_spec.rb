56108c5eb38be1fe50170493f0bd861991ed5406
Tim Riley <tim@riley.id.au>
2022-11-24 22:31:54 +1100

    Show latest cafes on the home page

--- spec/slices/main/features/home_page_spec.rb
+++ spec/slices/main/features/home_page_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe "Home page", :web do
+RSpec.describe "Home page", :web, :db do
   specify "Visiting the home page" do
+    factory[:cafe, name: "Group Seven"]
+    factory[:cafe, name: "Harvest"]
+
     visit "/"
+
+    expect(page).to have_content("Group Seven")
+    expect(page).to have_content("Harvest")
   end
 end
