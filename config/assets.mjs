9642e770ee2082f44615ea37ce0efd64963c1f73
Tim Riley <tim@riley.id.au>
2023-10-16 16:23:35 +1100

    Demonstrate PostCSS setup
    This runs PostCSS via an esbuild plugin configured in `config/assets.mjs`, then configures PostCSS to use Autoprefixer, and adds some CSS that will trigger Autoprefixer transformations when being compiled via `hanami assets`.

--- config/assets.mjs
+++ config/assets.mjs
@@ -1,3 +1,13 @@
 import * as assets from "hanami-assets";
+import postcss from "esbuild-postcss";
 
-await assets.run();
+await assets.run({
+  esbuildOptionsFn: (args, esbuildOptions) => {
+    const plugins = [...esbuildOptions.plugins, postcss()];
+
+    return {
+      ...esbuildOptions,
+      plugins,
+    };
+  },
+});
