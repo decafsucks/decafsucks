8719c98973ce561852096c9d1e34eec7a932173c
Tim Riley <tim@riley.id.au>
2023-10-16 17:05:28 +1100

    Add TypeScript (#27)
    Install TypeScript, update the main `.js` file to be `.ts`, and configure some TypeScript paths to make referencing files more straightforward.

--- slices/main/assets/js/app.js
+++
@@ -1,4 +0,0 @@
-import "../../../../app/assets/builds/tailwind.css";
-import "../css/app.css";
-
-console.log("Hello from Decaf Sucks");
---
+++ slices/main/assets/js/app.ts
@@ -0,0 +1,4 @@
+import "@app/builds/tailwind.css";
+import "@main/css/app.css";
+
+console.log("Hello from Decaf Sucks");
