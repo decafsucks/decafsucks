8c3e1818757c47208fdec4e1480ba730de2be667
Tim Riley <tim@riley.id.au>
2025-06-30 23:03:32 +0300

    Tidy

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,32 +3,38 @@
 
 require "vcr"
 
+# Finds any `:vcr`-tagged example and configures it with a `cassette_name` that puts the casette
+# alongside the related spec file.
+#
+# For example, for `spec/slices/main/foo_spec.rb` containing `it "returns true", :vcr`, the cassette
+# will be located at `spec/slices/main/foo_spec/returns_true.yml`.
 RSpec.configure do |config|
   config.before(:each, {vcr: ->(v) { !!v }}) do |example|
     metadata = example.metadata
-
     vcr_options = metadata[:vcr].is_a?(Hash) ? metadata[:vcr] : {}
+
+    # Do not override an explicitly configured cassette name.
     next if vcr_options.key?(:cassette_name)
 
-    # Only work on files located inside the "spec/" directory
-    relative_file_path_re = %r{^.?/?spec/}
-    next unless metadata[:file_path].match?(relative_file_path_re)
-
-    # Strip "./spec/" from beginning file_path (a file path looks like
-    # "./spec/slices/geocoding/lookups/location_iq_spec.rb")
-    relative_file_path = metadata[:file_path]
-      .sub(relative_file_path_re, "")
+    # Strip "./spec/" from the beginning of the example's file_path.
+    #
+    # This isn't needed since we assign "spec" to VCR's `cassette_library_dir` below.
+    relative_file_path = metadata[:file_path].sub(%r{^.?/?spec/}, "")
 
     vcr_options[:cassette_name] = [
       File.dirname(relative_file_path),
       File.basename(relative_file_path, File.extname(relative_file_path)),
-      example.metadata[:description].downcase # TODO make robust
+      example.metadata[:description].downcase
     ].join("/")
 
     example.metadata[:vcr] = vcr_options
   end
 end
 
+# Activates VCR when the `:vcr` example metadata is given.
+#
+# It's important that this comes _after_ the RSpec configuration above, so that our `cassette_name`
+# logic can apply first.
 VCR.configure do |config|
   config.cassette_library_dir = "spec"
   config.hook_into :faraday
