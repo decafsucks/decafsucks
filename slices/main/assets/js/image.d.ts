212ab67f2b7338b75ee2ab951d5309016650fba4
Tim Riley <tim@riley.id.au>
2025-12-26 14:53:56 +1100

    Keep image.d.ts as small as possible

--- slices/main/assets/js/image.d.ts
+++ slices/main/assets/js/image.d.ts
@@ -1,24 +1,4 @@
-declare module '*.png' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.jpeg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.svg' {
-  const value: string;
-  export default value;
-}
-
-declare module '*.gif' {
+declare module "*.png" {
   const value: string;
   export default value;
 }
