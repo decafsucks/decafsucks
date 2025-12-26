0a8af842ddf02131639a674c2e1820574439fa77
Tim Riley <tim@riley.id.au>
2025-12-26 14:42:08 +1100

    Tidy publicPath config

--- config/assets.js
+++ config/assets.js
@@ -12,8 +12,13 @@ await assets.run({
     // Use the `args.watch` boolean as a condition to apply diffierent options
     // when running `hanami assets watch` vs `hanami assets compile`.
 
-    // Set public path to ensure assets use absolute URLs
-    esbuildOptions.publicPath = "/assets/_main";
+    // Set public path to ensure assets use absolute URLs, detecting the slice
+    // name from the asset destination path.
+    //
+    // This is necessary for the Leaflet image paths to work. Later, we may
+    // consider setting publicPath as part of standard hanami-assets behavior.
+    const sliceName = args.dest.split("/").pop();
+    esbuildOptions.publicPath = `/assets/${sliceName}`;
 
     return esbuildOptions;
   },
