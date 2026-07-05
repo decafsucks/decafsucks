47de6859d17a409bdf6fed0af7234993da83f374
Tim Riley <tim@riley.id.au>
2026-07-05 14:50:08 +1000

    Update to Hanami 3.0 (#49)

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 Decaf Sucks was once a thriving little cafe review app and community.
 
-We’re going to rebuild it as an OSS Hanami 2.0 example application. Watch this space!
+We’re going to rebuild it as an open source Hanami app. Watch this space!
 
 ## Setup
 
--- config/puma.rb
+++ config/puma.rb
@@ -32,9 +32,6 @@ workers puma_concurrency
 #
 
 if puma_cluster_mode
-  # Preload the application before starting the workers. Only in cluster mode.
-  preload_app!
-
   # Code to run immediately before master process forks workers (once on boot).
   #
   # These hooks can block if necessary to wait for background operations unknown
